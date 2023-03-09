972d737e36021f107f49e10242d58724ca606278
lLotosl <lotosyt1@gmail.com>
2023-03-09 09:22:27 +0500

    Added custom pagination class.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'apps.core.pagination.CustomLimitOffsetPagination',
 }
 
 SIMPLE_JWT = {
