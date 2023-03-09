4573b3f7123fb2516c6ac95856bc31291908e141
lLotosl <lotosyt1@gmail.com>
2023-03-09 09:59:09 +0500

    Fixed bug with not working IsAdminUserOrReadOnly permission.

--- backend/apps/core/permissions.py
+++ backend/apps/core/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
         
